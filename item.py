0dbd4c8942015be4d4e2828925c41fac533af838
Prasanthi <prasanthi.prasanthi@capgemini.com>
2022-07-11 10:48:39 +0530

    commit2

--- item.py
+++ item.py
@@ -23,10 +23,14 @@ class Item:
     @property
     # Property Decorator = Read-only Attribute
     def name(self):
+            
         return self.__name
     
     @name.setter
     def name(self, value):
+        if len(value) > 10:
+            raise Exception('The name is too long!')
+        else:
             self.__name = value
     
     def calculate_total_price(self):
